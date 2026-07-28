f0561ba0540fdecb21ab1546577646a149eb7c17
Omar Ahmed <omar@example.com>
2026-07-28 07:21:39 +0000

    Rename module to smart_automated_reconcile_payment (unique technical name)

--- smart_automated_reconcile_payment/models/res_config_settings.py
+++ smart_automated_reconcile_payment/models/res_config_settings.py
@@ -39,10 +39,10 @@ class ResConfigSettings(models.TransientModel):
             "res_model": "smart.payment.match.customer",
             "view_mode": "tree",
             "view_id": self.env.ref(
-                "smart_payment_allocation.view_smart_payment_match_customer_list"
+                "smart_automated_reconcile_payment.view_smart_payment_match_customer_list"
             ).id,
             "search_view_id": self.env.ref(
-                "smart_payment_allocation.view_smart_payment_match_customer_search"
+                "smart_automated_reconcile_payment.view_smart_payment_match_customer_search"
             ).id,
             "context": {},
         }
